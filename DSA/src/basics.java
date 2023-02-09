14d44623c684eb29f0f370997dc5fec263d73595
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-02-09 23:05:19 +0530

    added some files

---
+++ DSA/src/basics.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class basics {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int a = (int)(64.34f);
+//        System.out.println(a);
+
+//        float num =input.nextFloat();
+//        int num = input.nextInt();
+//        System.out.println(num);
+
+//          int a =600;
+//          byte b= (byte)(a);     //  600 % 256=600
+//          System.out.println(b);
+
+      //  byte a = 40;
+//        byte b = 50;
+//        byte c = 100;
+//        int d = a * b / c;
+//
+//        System.out.println(d);
+
+
+    }
+}
