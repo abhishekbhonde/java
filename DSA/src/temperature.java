f0a9e21f7490ed32bebe32eb67bf5dad930e0417
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-02-09 23:11:03 +0530

    added files

--- DSA/src/temperature.java
+++ DSA/src/temperature.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class temperature {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter temp in degree celsious");
+        float tempC=input.nextFloat();
+        float tempF= (tempC*5/9) +32;
+        System.out.println("The tempreature in F is " + tempF);
+    }
 }
