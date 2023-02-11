9a0d74ca386ffbe8ee54dddeb3b238a4f7e9a446
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-02-11 23:34:37 +0530

    added factors

--- DSA/src/ConditionalsLoops/calculator.java
+++ DSA/src/ConditionalsLoops/calculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args) {
-        
+
         Scanner in = new Scanner(System.in);
         int ans = 0;
         while (true) {
