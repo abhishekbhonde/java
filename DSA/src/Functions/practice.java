27b6f5d70063bb70e545361c477ac307611ddf27
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-02-17 23:25:34 +0530

    add

---
+++ DSA/src/Functions/practice.java
@@ -0,0 +1,27 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class practice {
+
+    public static void main(String[] args) {
+        sum();
+
+        }
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+/*
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int ans = a+b;
+        System.out.println(ans);*/
+
+       /* String name = in.next();
+        System.out.println(name);
+*/
+        
+
+}
+
+
+}
