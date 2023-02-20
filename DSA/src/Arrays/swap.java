e7510e8a8cbcb4f5b2d8f1be0f57ee857fed7ef2
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-02-20 22:02:29 +0530

    added files

--- DSA/src/Arrays/swap.java
+++ DSA/src/Arrays/swap.java
@@ -5,9 +5,19 @@ import java.util.Arrays;
 public class swap {
     public static void main(String[] args) {
     int [] arr = {1,34,5432,234,234,};
-    swap(arr, 1, 3);
-
+    // swap(arr, 1, 3);
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int [] arr){
+        int start =0;
+        int end = arr.length-1;
+        while(start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
     }
     static void swap(int [] arr, int index1, int index2){
         int temp=arr[index1];
