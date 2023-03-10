f35ae966051be24fcd3601bcc7c6969a2c819a25
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-10 14:54:18 +0530

    new one

---
+++ DSA/src/Leetcode/SortColors75.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class SortColors75 {
+    public static void main(String[] args) {
+        int []arr ={2,0,2,1,1,0};
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubblesort(int [] arr){
+        boolean swapped;
+        for(int i =0; i<arr.length; i++){
+            swapped = false;
+            for(int j =1; j<arr.length-i; j++){
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
