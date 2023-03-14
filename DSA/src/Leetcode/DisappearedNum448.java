c6e651621907ded02938f033be9d47f34f77fc02
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-14 11:59:57 +0530

    added code

---
+++ DSA/src/Leetcode/DisappearedNum448.java
@@ -0,0 +1,39 @@
+package Leetcode;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisappearedNum448 {
+    public static void main(String[] args) {
+        int [] arr= {4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+
+    }
+    public static List<Integer> findDisappearedNumbers(int[] arr){
+        int i =0;
+        while(i<arr.length){
+            int correct = arr[i] -1;
+            if(arr[i]!=arr[correct]){
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int index =0; index < arr.length; index++){
+            if(arr[index]!=index+1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+
+    }
+    static void swap(int [] arr , int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+}
