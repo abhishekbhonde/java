75dd639f792ece4b03159f2fa37254e80209d036
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-16 23:42:32 +0530

    added new code

--- DSA/src/sortings/BubbleSort.java
+++ DSA/src/sortings/BubbleSort.java
@@ -12,11 +12,11 @@ public class BubbleSort {
         boolean swapped;
         for (int i =0; i<arr.length; i++){
            swapped=false;
-            for (int j=1; j<arr.length-i; j++){
+            for(int j=1; j<arr.length-i; j++){
                if(arr[j] < arr[j-1]){
                 int temp= arr[j];
                 arr[j]=arr[j-1];
-                   arr[j-1]= temp;
+                arr[j-1]= temp;
                 swapped=true;
             }
         }
