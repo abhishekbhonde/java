740f2f2ee269d870adb6f5b061e5b2df4e2cd021
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-21 13:02:18 +0530

    added String Builder

---
+++ DSA/src/Strings/SB.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class SB {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        for(int i = 0; i<26; i++){
+            char ch = (char)('a' + i);
+            builder.append(ch);
+
+
+        }
+        System.out.println(builder.toString());
+
+
+    }
+}
