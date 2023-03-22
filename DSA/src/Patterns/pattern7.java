c6d2653c7d9eb43309d11d6ae9db0748e0cea27b
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-22 23:59:01 +0530

    added pattern 7

---
+++ DSA/src/Patterns/pattern7.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
