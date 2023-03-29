98558cf7bdabb88cd10235f5e2119d27d53e7cc3
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-29 23:01:21 +0530

    added new files

--- DSA/src/Strings/BasicStrings.java
+++ DSA/src/Strings/BasicStrings.java
@@ -1,10 +1,14 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class BasicStrings {
     public static void main(String[] args) {
 
         String a  = "Abhishek";
 
         System.out.println(a);
+
+
     }
 }
