fa6545fc46d5e6560c429fcfd94745693f8f43ee
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-03-30 10:27:27 +0530

    added new recursion code

---
+++ DSA/src/Recursion/HelloWorld.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Helloworld();
+    }
+    static void Helloworld(){
+        System.out.println("Hello Abhishek");
+        Helloworld1();
+    }
+    static void Helloworld1(){
+        System.out.println("Hello Abhishek");
+        Helloworld2();
+    }static void Helloworld2(){
+        System.out.println("Hello Abhishek");
+        Helloworld3();
+    }static void Helloworld3(){
+        System.out.println("Hello Abhishek");
+        Helloworld4();
+    }static void Helloworld4(){
+        System.out.println("Hello Abhishek");
+    }
+
+
+
+
+
+
+}
