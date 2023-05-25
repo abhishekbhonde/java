f551412f4a459f6eca4f31b0a5e25b9133ac1830
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-05-25 22:54:19 +0530

    new one

---
+++ src/Leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + maxProfit);
+    }
+}
