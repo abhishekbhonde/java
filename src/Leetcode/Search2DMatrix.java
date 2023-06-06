ea56984885728f5e8eeaec325cab9c122191298d
abhishekbhonde <abhishekbhonde567@gmail.com>
2023-06-06 23:48:03 +0530

    new code added

---
+++ src/Leetcode/Search2DMatrix.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+public class Search2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int row = 0;
+        int col = cols - 1;
+
+        while (row < rows && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Search2DMatrix searchMatrix = new Search2DMatrix();
+
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+
+        int target = 3;
+        boolean found = searchMatrix.searchMatrix(matrix, target);
+        System.out.println("Target found: " + found);
+    }
+}
